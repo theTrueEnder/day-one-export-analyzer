8f2e7e0e5c5f2d938eea0de6e05a4419e846e265
theTrueEnder <67336179+theTrueEnder@users.noreply.github.com>
2024-08-08 19:32:58 -0400

    Type annotations and set addition/subtraction
    Added type annotations for arguments and return types when possible
    Added __str__() functions for Journal and Entry
    Default option for _get_multi()
    Expanded getters in Entry
    Finished prototyping filter by tags and keyword

--- entry.py
+++ entry.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 import pytz
 
-def convert_to_local(tz, timestr):
+def convert_to_local(tz: str, timestr: str)->datetime:
     utc_time = datetime.strptime(timestr, "%Y-%m-%dT%H:%M:%SZ")
     timezone = pytz.timezone(tz)
     
@@ -11,16 +11,19 @@ def convert_to_local(tz, timestr):
 
 
 class Entry():
-    def __init__(self, e):
+    def __init__(self, e: dict):
         self._tz = e.get('timeZone').replace('\\/', '/')
         self._created = convert_to_local(self._tz, e.get('creationDate'))
         self._edit_duration = timedelta(seconds=e.get('editingTime'))
         self._completed = self._created + self._edit_duration
         self._uuid = e.get('uuid')
-        self._pinned = e.get('isPinned')
-        self._starred = e.get('starred')
-        self._text = e.get('text')
-        self._rich_text = e.get('richText')
+        self._pinned = e.get('isPinned', False)
+        self._starred = e.get('starred', False)
+        self._tags = e.get('tags', [])
+        self._text = e.get('text', '')
+        self._rich_text = e.get('richText', '')
+        
+        
         self._country = self._get_multi(e, 'location', 'country')
         self._admin_area = self._get_multi(e, 'location', 'administrativeArea')
         self._locality = self._get_multi(e, 'location', 'localityName')
@@ -28,43 +31,65 @@ class Entry():
         self._long = self._get_multi(e, 'location', 'longitude')
         self._placename = self._get_multi(e, 'location', 'placeName')
     
-    def _get_multi(self, d, *keys):
+    def __str__(self)->str:
+        s = 'Entry object:\n\t'
+        s += f'- UUID: {self._uuid}\n\t'
+        s += f'- Created: {self._created}\n\t'
+        s += f'- Pinned: {self._pinned}\n\t'
+        s += f'- Starred: {self._starred}\n\t'
+        s += f'- Tags: {self._tags}\n\t'
+        if len(self._text > 10):
+            s += f'- Text: "{self._text[:10]}..."\n\t'
+        else:
+            s += f'- Text: "{self._text}"\n\t'
+    
+    def _get_multi(self, d: dict, default=None, *keys: str)->any:
         val = d.get(keys[0])
         for key in keys:
             if val is None:
                 break
             val = val.get(key)
-        return val
+        
+        if val is None:
+            return default
+        else:
+            return val
     
-    def get_created_time(self):
+    def get_created_time(self)->datetime:
         return self._created
         
-    def get_completed_time(self):
+    def get_completed_time(self)->datetime:
         return self._completed
     
-    def is_starred(self):
+    def is_starred(self)->bool:
         return self._starred
         
-    def is_pinned(self):
+    def is_pinned(self)->bool:
         return self._pinned
-    
-    def get_text(self):
-        ...
         
-    def get_rich_text(self):
-        ...
-        
-    def get_coordinates(self):
+    def get_coordinates(self)->tuple[float, float]:
         return (self._lat, self._long)
     
-    def get_country(self):
+    def get_country(self)->str:
         return self._country
     
-    def get_admin_area(self):
+    def get_admin_area(self)->str:
         return self._admin_area    
         
-    def get_locality(self):
+    def get_locality(self)->str:
         return self._locality
     
-    def get_placename(self):
+    def get_placename(self)->str:
         return self._placename
+    
+    def get_tags(self)->list[str]:
+        return self._tags
+    
+    def get_text(self)->str:
+        return self._text
+    
+    def get_rich_text(self)->str:
+        return self._rich_text
+    
+    def get_uuid(self)->str:
+        return self._uuid
--- journal.py
+++ journal.py
@@ -7,29 +7,48 @@ SUCCESS_CODE = 1
 
 class Journal():
     def __init__(self, entries=[]):
-        self.entries = entries
+        self.uuid_entries = {}
+        [self.uuid_entries.update({"uuid": entry.get_uuid(), "entry": entry}) for entry in entries]
+        self._update_entries(entries)
         
-    def __str__(self):
-        ...
-        
-    def __sub__(self, other):
-        ...
+    def __str__(self)->str:
+        s = f'Journal object:\n\t{self.get_entry_count()} entries:\n\t'
+        for entry in self.entries:
+            s += str(entry)
+        return s
         
     def __add__(self, other):
-        ...
+        [self.uuid_entries.update({entry.get_uuid(): entry}) for entry in other.get_entries()]
+        self.entries = list(self.uuid_entries.keys())
+        return self
+        
+    def __sub__(self, other):
+        [self.uuid_entries.pop(entry.get_uuid(), None) for entry in other.get_entries()]
+        self.entries = list(self.uuid_entries.keys())
+        return self
         
         
-    def get_entry_count(self):
-        return len(self.entries)
+    def _update_entries(self, updated_entries):
+        self.uuid_entries.clear()
+        [self.uuid_entries.update({entry.get_uuid(): entry}) for entry in updated_entries]
+        self.entries = updated_entries
         
-    def _return_filter_result(filtered_entries):
+    def _return_filter_result(filtered_entries: list[Entry]):
         e_ct = len(filtered_entries)
         if e_ct == 0:
             return {"code": NO_MATCH_CODE, "msg": NO_MATCH_MSG, "count": 0}
         else:
             return {"code": SUCCESS_CODE, "msg": filtered_entries, "count": e_ct}
             
-    def filter_starred(self, starred=True, in_place=False):
+            
+            
+    def get_entry_count(self)->int:
+        return len(self.entries)
+    
+    def get_entries(self)->list[Entry]:
+        return self.entries
+    
+    def filter_starred(self, starred=True, in_place=False)->dict:
         if starred:
             entries = [entry for entry in self.entries if entry.is_starred()]
         else:
@@ -37,10 +56,10 @@ class Journal():
     
         res = self._return_filter_result(entries)
         if res['code'] == SUCCESS_CODE and in_place:
-            self.entries = entries
+            self._update_entries(entries)
         return res
         
-    def filter_pinned(self, pinned=True, in_place=False):
+    def filter_pinned(self, pinned=True, in_place=False)->dict:
         if pinned:
             entries = [entry for entry in self.entries if entry.is_pinned()]
         else:
@@ -48,20 +67,20 @@ class Journal():
             
         res = self._return_filter_result(entries)
         if res['code'] == SUCCESS_CODE and in_place:
-            self.entries = entries
+            self._update_entries(entries)
         return res
         
-    def filter_by_country(self, country, negative=False, in_place=False):
+    def filter_by_country(self, country: str, negative=False, in_place=False)->dict:
         if not negative:
             entries = [entry for entry in self.entries if country == entry.get_country()]
         else:
             entries = [entry for entry in self.entries if country == entry.get_country()]
         res = self._return_filter_result(entries)
         if res['code'] == SUCCESS_CODE and in_place:
-            self.entries = entries
+            self._update_entries(entries)
         return res
         
-    def filter_by_admin_area(self, admin_area, negative=False, in_place=False):
+    def filter_by_admin_area(self, admin_area: str, negative=False, in_place=False)->dict:
         if not negative:
             entries = [entry for entry in self.entries if admin_area == entry.get_admin_area()]
         else:
@@ -69,10 +88,10 @@ class Journal():
             
         res = self._return_filter_result(entries)
         if res['code'] == SUCCESS_CODE and in_place:
-            self.entries = entries
+            self._update_entries(entries)
         return res
         
-    def filter_by_locality(self, locality, negative=False, in_place=False):
+    def filter_by_locality(self, locality: str, negative=False, in_place=False)->dict:
         if not negative:
             entries = [entry for entry in self.entries if locality == entry.get_locality()]
         else:
@@ -80,10 +99,10 @@ class Journal():
             
         res = self._return_filter_result(entries)
         if res['code'] == SUCCESS_CODE and in_place:
-            self.entries = entries
+            self._update_entries(entries)
         return res
         
-    def filter_by_placename(self, placename, negative=False, in_place=False):
+    def filter_by_placename(self, placename: str, negative=False, in_place=False)->dict:
         if not negative:
             entries = [entry for entry in self.entries if placename == entry.get_placename()]
         else:
@@ -91,27 +110,38 @@ class Journal():
             
         res = self._return_filter_result(entries)
         if res['code'] == SUCCESS_CODE and in_place:
-            self.entries = entries
+            self._update_entries(entries)
         return res
         
-    def filter_by_tags(self, tags=[], negative=False, in_place=False):
-        if not negative:
-            entries = ...
-        else:
-            entries = ...
+    def filter_by_tags(self, tags=[], negative=False, match_all=False, in_place=False)->dict:
+        # if not negative:
+        tagset = set(tags)
+        entries = []
+        for entry in self.entries:
+            etagset = set(entry.get_tags())
+            if not negative:
+                if match_all and tagset.issubset(etagset):
+                    entries.append(entry)
+                elif not match_all and tagset.issubset(etagset):
+                    entries.append(entry) 
+            else:
+                if match_all and not tagset.issubset(etagset):
+                    entries.append(entry)
+                elif not match_all and not tagset.issubset(etagset):
+                    entries.append(entry) 
             
         res = self._return_filter_result(entries)
         if res['code'] == SUCCESS_CODE and in_place:
-            self.entries = entries
+            self._update_entries(entries)
         return res
         
-    def filter_by_keyword(self, keyword, negative=False, in_place=False):
+    def filter_by_keyword(self, keyword: str, negative=False, in_place=False)->dict:
         if not negative:
-            entries = ...
+            entries = [entry for entry in self.entries if keyword in entry.get_text()]
         else:
-            entries = ...
+            entries = [entry for entry in self.entries if keyword not in entry.get_text()]
             
         res = self._return_filter_result(entries)
         if res['code'] == SUCCESS_CODE and in_place:
-            self.entries = entries
+            self._update_entries(entries)
         return res
