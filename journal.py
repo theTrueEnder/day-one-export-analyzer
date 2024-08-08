75bbbedb5e18a1a1717a93fe674f4f17d00efbd8
theTrueEnder <67336179+theTrueEnder@users.noreply.github.com>
2024-08-08 17:49:01 -0400

    Added negative filtering
    Fixed naming error for filter_pinned
    Added structure for set addition/subtraction for journal objects
    Added negative filtering where possible

--- journal.py
+++ journal.py
@@ -12,6 +12,13 @@ class Journal():
     def __str__(self):
         ...
         
+    def __sub__(self, other):
+        ...
+        
+    def __add__(self, other):
+        ...
+        
+        
     def get_entry_count(self):
         return len(self.entries)
         
@@ -23,56 +30,87 @@ class Journal():
             return {"code": SUCCESS_CODE, "msg": filtered_entries, "count": e_ct}
             
     def filter_starred(self, starred=True, in_place=False):
-        entries = [entry for entry in self.entries if entry.is_starred()]
+        if starred:
+            entries = [entry for entry in self.entries if entry.is_starred()]
+        else:
+            entries = [entry for entry in self.entries if not entry.is_starred()]
+    
         res = self._return_filter_result(entries)
         if res['code'] == SUCCESS_CODE and in_place:
             self.entries = entries
         return res
         
-    def filter_pinned(self, starred=True, in_place=False):
-        entries = [entry for entry in self.entries if entry.is_pinned()]
+    def filter_pinned(self, pinned=True, in_place=False):
+        if pinned:
+            entries = [entry for entry in self.entries if entry.is_pinned()]
+        else:
+            entries = [entry for entry in self.entries if not entry.is_pinned()]
+            
         res = self._return_filter_result(entries)
         if res['code'] == SUCCESS_CODE and in_place:
             self.entries = entries
         return res
         
-    def filter_by_country(self, country, in_place=False):
-        entries = [entry for entry in self.entries if country == entry.get_country()]
+    def filter_by_country(self, country, negative=False, in_place=False):
+        if not negative:
+            entries = [entry for entry in self.entries if country == entry.get_country()]
+        else:
+            entries = [entry for entry in self.entries if country == entry.get_country()]
         res = self._return_filter_result(entries)
         if res['code'] == SUCCESS_CODE and in_place:
             self.entries = entries
         return res
         
-    def filter_by_admin_area(self, admin_area, in_place=False):
-        entries = [entry for entry in self.entries if admin_area == entry.get_admin_area()]
+    def filter_by_admin_area(self, admin_area, negative=False, in_place=False):
+        if not negative:
+            entries = [entry for entry in self.entries if admin_area == entry.get_admin_area()]
+        else:
+            entries = [entry for entry in self.entries if admin_area != entry.get_admin_area()]
+            
         res = self._return_filter_result(entries)
         if res['code'] == SUCCESS_CODE and in_place:
             self.entries = entries
         return res
         
-    def filter_by_locality(self, locality, in_place=False):
-        entries = [entry for entry in self.entries if locality == entry.get_locality()]
+    def filter_by_locality(self, locality, negative=False, in_place=False):
+        if not negative:
+            entries = [entry for entry in self.entries if locality == entry.get_locality()]
+        else:
+            entries = [entry for entry in self.entries if locality != entry.get_locality()]
+            
         res = self._return_filter_result(entries)
         if res['code'] == SUCCESS_CODE and in_place:
             self.entries = entries
         return res
         
-    def filter_by_placename(self, placename, in_place=False):
-        entries = [entry for entry in self.entries if placename == entry.get_placename()]
+    def filter_by_placename(self, placename, negative=False, in_place=False):
+        if not negative:
+            entries = [entry for entry in self.entries if placename == entry.get_placename()]
+        else:
+            entries = [entry for entry in self.entries if placename != entry.get_placename()]
+            
         res = self._return_filter_result(entries)
         if res['code'] == SUCCESS_CODE and in_place:
             self.entries = entries
         return res
         
-    def filter_by_tags(self, tags=[], in_place=False):
-        entries = ...
+    def filter_by_tags(self, tags=[], negative=False, in_place=False):
+        if not negative:
+            entries = ...
+        else:
+            entries = ...
+            
         res = self._return_filter_result(entries)
         if res['code'] == SUCCESS_CODE and in_place:
             self.entries = entries
         return res
         
-    def filter_by_keyword(self, keyword, in_place=False):
-        entries = ...
+    def filter_by_keyword(self, keyword, negative=False, in_place=False):
+        if not negative:
+            entries = ...
+        else:
+            entries = ...
+            
         res = self._return_filter_result(entries)
         if res['code'] == SUCCESS_CODE and in_place:
             self.entries = entries
